18e9674ff9754a5d78a7af934e06af784481a5b2
Brandon West <brandon@brandonwest.me>
2014-06-26 11:56:16 -0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -52,7 +52,7 @@
         _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody
                                                               bodyB:_catapultArm.physicsBody
                                                             anchorA:ccp(0, 0)
-                                                            anchorB:ccp(14, 150)
+                                                            anchorB:ccp(34, 138)
                                                          restLength:0.f stiffness:3000.f
                                                             damping:150.f];
         
